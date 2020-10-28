3355584c462c9c65788f6f85ab4b5147d0c4a84c
Nathalie D-Bonheur <n.durassierbonheur@epsi.fr>
2020-10-28 14:06:17 +0100

    TP1,2,3

--- src/main/java/essais/TestPersonne.java
+++ src/main/java/essais/TestPersonne.java
@@ -4,9 +4,11 @@ import entites.Personne;
 
 public class TestPersonne {
     public static void main(String[] args){
-        Personne personne1 = new Personne('BLO', 'blo');
-        Personne personne2 = new Personne('BLA', 'bla');
+        Personne personne1 = new Personne("BLO", "blo", "1 Rue de Blo");
+        Personne personne2 = new Personne("BLA", "bla", "3 Rue de Bla");
         System.out.println(personne1);
         System.out.println(personne2);
+        personne1.afficherPersonne(personne1);
+        personne1.setNom("toto");
     }
 }
